93dac38183621b31e0b64b12b03652ae2c9be69c
Billy Watson <nextbillyonair@gmail.com>
2021-07-19 14:11:36 -0400

    Adding initial codebase for website assets

---
+++ app.js
@@ -0,0 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'https://raw.githubusercontent.com/aublhr99/aublhr99.github.io/master/assets/particlesjs-config.json', function() {
+  console.log('callback - particles.js config loaded');
+});
